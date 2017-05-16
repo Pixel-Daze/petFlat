ec40ba2d5d7a12c6352c5ebe28dc6071bb869a67
Pixel-Daze <1226093323@qq.com>
2017-05-17 00:27:44 +0800

    收藏删除

--- src/components/petMine/MineCollect.vue
+++ src/components/petMine/MineCollect.vue
@@ -2,7 +2,7 @@
 	<div class="mineCollect">
 		<x-header class="vux-1px-b fix-header" :left-options="{backText: ''}" @on-click-back="back">我的收藏</x-header>
 		<div class="pet_container">
-			<pet-list :petList="PetList" @choosePet="choosePet"></pet-list>
+			<pet-list :petList="PetList" @choosePet="choosePet" @delelePet="delelePet"></pet-list>
 		</div>
 	</div>
 </template>
@@ -34,6 +34,30 @@
 			},
 			choosePet(item){
 		    	this.$router.push({name:'PetDetail',params:{petCode:item.PetCode}})
+		    },
+		    delelePet(item){
+		    	let vm = this
+		    	vm.$vux.confirm.show({
+        		  	content: '确定删除此条宠物信息吗',
+				  	onCancel () {
+				  	},
+				  	onConfirm () {
+				  		let body={
+				  			PetCode:item.PetCode
+				  		}
+				  		api.delelePet(body).then(resp=>{
+				  			if(resp.data.result == 0){
+				  				vm.$vux.toast.show({
+									text: '删除成功',
+									width:'9em',
+									type: 'text'
+								})
+								vm.page = 1
+								vm.getIndexList()
+				  			}
+				  		})
+				  	}
+				})
 		    }
 		},
 		created(){
